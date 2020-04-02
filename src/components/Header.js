6eee4a11fec3d00f0877e750c9d1cb9112842f13
lizlove <elizabethlovero@gmail.com>
2020-04-02 18:55:09 -0400

    Basic landing.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,43 @@
-import React from 'react'
+import React from "react";
 
 class Header extends React.Component {
-    render() {
-        return (
-            <section id="header">
-                <div className="inner">
-                    <span className="icon major fa-cloud"></span>
-                    <h1>Hi, I'm <strong>Photon</strong>, another fine<br />
-                    little freebie from <a href="http://html5up.net">HTML5 UP</a>.</h1>
-                    <p>Accumsan feugiat mi commodo erat lorem ipsum, sed magna<br />
-                    lobortis feugiat sapien sed etiam volutpat accumsan.</p>
-                    <ul className="actions">
-                        <li><a href="#one" className="button scrolly">Discover</a></li>
-                    </ul>
-                </div>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section id="header">
+        <div className="inner">
+          <span className="icon major fa-heart"></span>
+          <h1>
+            Oh, hello.
+            <br />
+            It seems you are another fine
+            <br />
+            friend of{" "}
+            <a href="http://www.pistilandstamenflowers.com/">
+              Megan and Denise
+            </a>
+            .
+          </h1>
+          <p>
+            While we cannot celebrate live oak love together in NOLA,
+            <br />
+            we can still come together to gather gifts and good wishes.
+          </p>
+          <ul className="actions">
+            <li>
+              <a href="#one" className="button scrolly">
+                Video 📹
+              </a>
+            </li>
+            <li>
+              <a href="#two" className="button scrolly">
+                Donate 💰
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    );
+  }
 }
 
-export default Header
+export default Header;
